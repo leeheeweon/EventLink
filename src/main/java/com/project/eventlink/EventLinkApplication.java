2b397e234433f223e702e67b27efb7508d3a75c2
kadun1 <kadunjj@gmail.com>
2023-11-13 22:10:27 +0900

    chor: project setting
     - set springboot project
      - springboot3
      - java 17
      - spring security
      - spring data jpa
      - thymeleaf
      - validation
      - flyway
      - postgresql
      - restdocs
      - lombok

---
+++ src/main/java/com/project/eventlink/EventLinkApplication.java
@@ -0,0 +1,13 @@
+package com.project.eventlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventLinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventLinkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/eventlink/EventLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.eventlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventLinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
